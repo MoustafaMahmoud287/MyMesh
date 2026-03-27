77abedf0fc4d46f7651acbce7e38504ccac68417
Moustafa Mahmoud <moustafa.mahmoud.latif@gmail.com>
2026-03-27 14:58:33 +0200

    feat: implemented face area function

--- src/GeometryMath.cpp
+++ src/GeometryMath.cpp
@@ -5,10 +5,22 @@ namespace myMesh {
 
         float faceArea(const Geometry::Mesh& mesh, Geometry::FaceHandle face) {
             
+            auto it = mesh.surroundingVertices(face).begin();
+
+            Point vertex1position = mesh.getVertexCopy(*it); ++it;
+            Point vertex2position = mesh.getVertexCopy(*it); ++it;
+            Point vertex3position = mesh.getVertexCopy(*it);
+
+            Point edge1 = vertex2position - vertex1position;
+            Point edge2 = vertex3position - vertex1position;
+
+            float area = 0.5f * cross(edge1, edge2).length();
+
+            return area;
         }
 
         float barycentricDualArea(const Geometry::Mesh& mesh, Geometry::VertexHandle vertex) {
-
+            return 0.0f;
         }
 
     }
